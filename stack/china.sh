4b55ba923d70ab79f72e5a99dc0d4a34f737b8ff
purerosefallen <78877@qq.com>
2021-01-07 22:02:34 +0800

    resolv

--- stack/china.sh
+++ stack/china.sh
@@ -4,5 +4,6 @@ apt-get update
 apt-get -y install docker.io python3-pip
 /usr/bin/pip3 install -U pip
 /usr/local/bin/pip install -U docker-compose
+echo 'nameserver 114.114.114.114' > /etc/resolv.conf
 wget -O docker-compose.yml https://water.mycard.moe:9000/nanahira/wolfx/docker-compose-china.yml
 docker-compose up
