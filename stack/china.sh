73821b8e270a5021b3f7ede24645b38f22f58095
purerosefallen <78877@qq.com>
2021-01-07 21:40:55 +0800

    pack

---
+++ stack/china.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# curl https://water.mycard.moe:9000/nanahira/wolfx/run.sh | sudo bash -
+apt-get update
+apt-get -y install docker.io python3-pip
+/usr/bin/pip3 install -U pip
+/usr/local/bin/pip install -U docker-compose
+wget -O docker-compose.yml https://water.mycard.moe:9000/nanahira/wolfx/docker-compose-china.yml
+docker-compose up
